7eb1e0c51aafdcf508e0f8939a5ad2545f9a9dc8
Md Junayed <devjunayed@gmail.com>
2025-04-30 21:54:28 +0600

    passowrd encrypted and token is generated

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,17 +1,49 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { tokenToString } from "typescript";
 
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
+const prisma = new PrismaClient();
 
-export  const resolvers = {
+export const resolvers = {
   Query: {
-    books: () => books,
+    users: async () => {
+      const result = await prisma.user.findMany();
+      return result;
+    },
+    user: async (parent: any, args: any, context: any) => {
+      console.log(args);
+      const result = await prisma.user.findFirst({
+        where: {
+          id: Number(args.id),
+        },
+      });
+      return result;
+    },
+  },
+  Mutation: {
+    signup: async (parent: any, args: any, context: any) => {
+      const hashedPassword = await bcrypt.hash(args.password, 12);
+
+      const result = await prisma.user.create({
+        data: {
+          ...args,
+          password: hashedPassword,
+        },
+      });
+
+      const generatedToken = jwt.sign(
+        result,
+        "a13a33e76be205cce064a1163a044491c2b7f4b0ff940550a9052fbc85bbe0e0",
+        {
+          expiresIn: "1d",
+        }
+      );
+
+      return {
+        data: result,
+        token: generatedToken
+      };
+    },
   },
 };
--- src/schema.ts
+++ src/schema.ts
@@ -1,12 +1,43 @@
 export const typeDefs = `#graphql
- 
-  type Book {
-    title: String
-    author: String
-  }
+ type Post{
+    id: ID!
+    title: String!
+    content: String!
+    author: User!
+    published: Boolean!
+    createdAt: String!
+ }
 
- 
-  type Query {
-    books: [Book]
-  }
+ type User{
+    id: ID!
+    name: String!
+    email: String!
+    posts: [Post]
+    createdAt: String!
+ }
+
+ type Profile{
+    id: ID!
+    bio: String!
+    createdAt: String!
+    user: User!
+ }
+
+ type UserArgs{
+      token: String
+      data: User
+ }
+
+
+ type Query{
+    posts: [Post!]!
+    users: [User!]!
+    user(id: String!): User!
+    profiles: [Profile!]!
+
+ }
+
+ type Mutation{
+   signup(name: String!, email: String!, password: String!): UserArgs!
+ }
 `;
